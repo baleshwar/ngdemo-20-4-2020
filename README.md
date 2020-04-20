07a3bcbcdf16fae7e667e5990d02eb07cfebd762
baleshwar <baleshwar@corfuit.com>
2020-04-20 19:09:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngdemo-20-4-2020
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngdemo-20-4-2020)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  datetime = '2020-04-11T00:00:00.000Z';
+}
